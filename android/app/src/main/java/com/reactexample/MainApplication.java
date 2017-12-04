9529bf1dca42d349b5fa5bd6cb05215c4be63123
Patrik Tesar <patrik@xyndata.com>
2017-12-04 07:09:41 +0100

    update

--- android/app/src/main/java/com/reactexample/MainApplication.java
+++ android/app/src/main/java/com/reactexample/MainApplication.java
@@ -1,12 +1,10 @@
-package io.realm.react.example;
+package com.reactexample;
 
 import android.app.Application;
-import android.util.Log;
 
 import com.facebook.react.ReactApplication;
 import com.airbnb.android.NativeNavigationPackage;
 import io.realm.react.RealmReactPackage;
-import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -15,13 +13,11 @@ import com.facebook.soloader.SoLoader;
 import java.util.Arrays;
 import java.util.List;
 
-import io.realm.react.RealmReactPackage;
-
 public class MainApplication extends Application implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
     @Override
-    protected boolean getUseDeveloperSupport() {
+    public boolean getUseDeveloperSupport() {
       return BuildConfig.DEBUG;
     }
 
@@ -30,9 +26,13 @@ public class MainApplication extends Application implements ReactApplication {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
             new NativeNavigationPackage(),
-            new RealmReactPackage(),
-          new RealmReactPackage()
+            new RealmReactPackage()
       );
+    }
+
+    @Override
+    protected String getJSMainModuleName() {
+      return "index";
     }
   };
 
